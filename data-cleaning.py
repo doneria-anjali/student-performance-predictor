bdb5381841531894c71ce798c3aa3255e7a6bcc4
doneria-anjali <anjali.doneria@gmail.com>
2018-12-02 21:42:07 -0500

    cleaning code

--- data-cleaning.py
+++ data-cleaning.py
@@ -11,106 +11,107 @@ def add_to_table(df, engine, final_name):
     df.to_sql(name=final_name, con=engine, if_exists='append', index=False)
     
 def clean_student_data():
-    student_df, engine = read_table('algebraTrain')
+    student_df, engine = read_table('algebraTrainSample2')
     print("Records fetched = " + str(len(student_df)))
     
-    #for count in range(len(student_df)):
-    for count in range(5):
-        record = student_df.iloc[count]
+    for count in range(len(student_df)):
+        record = student_df.loc[[count]]
+        #print(type(record))
         df = create_final_df(record)
         add_to_table(df, engine, 'algebraTrain_clean')
         
-    print("Finished inserting data to purchase_data")
+    print("Finished inserting data to clean table")
 
 
 def create_final_df(record):
-    record = pd.DataFrame(columns=['rowNumber','anonStudentId','problemHierarchy',
-                               'problemName','problemView',
-                               'stepName','stepStartTime','firstTransactionTime',
-                               'correctTransactionTime','stepEndTime',
-                               'stepDuration','correctStepDuration', 
-                               'errorStepDuration','correctFirstAttempt',
-                               'incorrects','hints','corrects','kc',
-                               'opportunity'])
-    
+    print(record)
     #clean data
-    row = record['rowNumber']
-    
-    anon_student_id = record['anonStudentId']    
-    record['problemHierarchy'] = record['problemHierarchy'].astype('str')
-    print('problem hierarchy' + record['problemHierarchy'])
-    
-    Problem_Name = record['problemName']
-    Problem_View = record['problemView']
-    Step_Name = record['stepName']
-    
-    Step_Start_Time = record['stepStartTime'].to_string()
-    First_Transaction_Time = record['firstTransactionTime'].to_string()
-    Step_Duration = record['stepDuration'].to_string()
-    Step_End_Time = record['stepEndTime'].to_string()
-    if not Step_Start_Time:
-        if First_Transaction_Time:
-            Step_Start_Time = First_Transaction_Time
-            Step_Duration = str(int(Step_End_Time) - int(Step_Start_Time))
-    
-    print(Step_Start_Time)
-    print(Step_Duration)
-    
-    Correct_Transaction_Time = record['correctTransactionTime']
-    Correct_Step_Duration = record['correctStepDuration']
-    Error_Step_Duration= record['errorStepDuration']
-    Correct_First_Attempt = record['correctFirstAttempt']
-    Incorrects = record['incorrects']
-    Hints = record['hints']
-    Corrects = record['corrects']
-    KC_default = record['kc'].to_string()
-    Opportunity_default = record['opportunity']
-    
-    ph1 = (record['problemHierarchy'] + '.')[:-1]
-    
-    unit = record['problemHierarchy'].split(',')[0].split('Unit')[1].strip()
-    prob = ph1.split(',')[1].split('-')[1]
-    
-    #pd.options.display.max_colwidth=500
-    kc_final = "empty"
-    if KC_default:
-        wordlist = []
-        kc = set()
-        wordlist = KC_default.split('~~')
-        str1 = ''
-        for word in wordlist:
-          str1 = word.strip().split('[SkillRule:')[1]
-          str1 = str1.split(';')[0].strip()
-          kc.add(str1)
-          
-        kc_final = str(kc)
-        print(kc_final)
-     
-    opp = Opportunity_default.split('~~')
-    opp_set = set()
-    for opportunity in opp:
-        str2 = opportunity.strip()
-        opp_set.add(str2)
-      
-    print(opp_set)
-    
-    #insert data to dataframe
-    list_data = pd.DataFrame(data = [[row, anon_student_id, 
-                               unit,prob,Problem_Name,Problem_View,
-                               Step_Name, Step_Start_Time, First_Transaction_Time,
-                               Correct_Transaction_Time, Step_End_Time,
-                               Step_Duration, Correct_Step_Duration, 
-                               Error_Step_Duration, Correct_First_Attempt,
-                               Incorrects, Hints, Corrects, str(opp_set), kc_final]],
-                               columns=['rowNumber','anonStudentId','unit','problem',
-                               'problemName','problemView',
-                               'stepName','stepStartTime','firstTransactionTime',
-                               'correctTransactionTime','stepEndTime',
-                               'stepDuration','correctStepDuration', 
-                               'errorStepDuration','correctFirstAttempt',
-                               'incorrects','hints','corrects',
-                               'opportunity','kc'])
-    record = record.append(list_data, ignore_index=True)
-    return list_data
+    try:
+        anon_student_id = record['anonStudentId'].to_string().split()[1].strip()
+        #print(anon_student_id[0])
+        #print(anon_student_id[1])
+        ProblemHierarchy = record['problemHierarchy'].to_string()
+        #print('problem hierarchy' + record['problemHierarchy'])
+        
+        """
+        Problem_Name = record['problemName'].to_string().strip()[1]
+        Problem_View = record['problemView'].to_string().strip()[1]
+        Step_Name = record['stepName'].to_string().strip()[1]
+        
+        Step_Start_Time = record['stepStartTime'].to_string().strip()[1]
+        First_Transaction_Time = record['firstTransactionTime'].to_string().strip()[1]
+        Step_Duration = record['stepDuration'].to_string().strip()[1]
+        Step_End_Time = record['stepEndTime'].to_string().strip()[1]
+        if not Step_Start_Time:
+            if First_Transaction_Time:
+                Step_Start_Time = First_Transaction_Time
+                Step_Duration = str(int(Step_End_Time) - int(Step_Start_Time))
+        
+        print('Step start time' + str(Step_Start_Time))
+        print('step duration' + str(Step_Duration))
+        
+        Correct_Transaction_Time = record['correctTransactionTime'].to_string().strip()[1]
+        Correct_Step_Duration = record['correctStepDuration'].to_string().strip()[1]
+        Error_Step_Duration= record['errorStepDuration'].to_string().strip()[1]
+        
+        Incorrects = record['incorrects'].to_string().strip()[1]
+        Hints = record['hints'].to_string().strip()[1]
+        Corrects = record['corrects'].to_string().strip()[1]
+        Opportunity_default = record['opportunity'].to_string().strip()[1]
+        """
+        
+        KC_default = record['kc'].to_string()
+        Correct_First_Attempt = record['correctFirstAttempt'].to_string().split()[1].strip()
+        
+        unit = ProblemHierarchy.split(',')[0].split()[2]
+        #print('Unit' + str(unit))
+        prob = ProblemHierarchy.split(',')[1].split('-')[1]
+        #print('Problem' + str(prob))
+        #prob = ProblemHierarchy.split(',')[1].split('-')[1]
+        
+        #pd.options.display.max_colwidth=500
+        kc_final = ''
+        if KC_default:
+            wordlist = []
+            kc = set()
+            wordlist = KC_default.split('~~')
+            str1 = ''
+            for word in wordlist:
+                if 'SkillRule' in word:
+                  str1 = word.strip().split('[SkillRule:')[1]
+                  str1 = str1.split(';')[0].strip()
+                  if str1 not in kc:
+                      kc_final += str1 + ','
+                      kc.add(str1)
+                else:
+                  print('in else')
+                  str1 = word.split(' ',1)[1].split('~~')
+                  for string in str1:
+                      if string not in kc:
+                          kc_final += str1 + ','
+                          kc.add(str1)
+                
+            kc_final = kc_final[:-1]
+        else:
+            kc_final = 'empty'      
+            
+        #print(kc_final)
+         
+        
+        #insert data to dataframe
+        list_data = pd.DataFrame(data = [[anon_student_id, 
+                                   unit,prob, Correct_First_Attempt, kc_final]],
+                                   columns=['anonStudentId','unit','problem',
+                                   'correctFirstAttempt','kc'])
+        #print(list_data)
+        record.append(list_data, ignore_index=True)
+        return list_data
+    except:
+        print('exception occured')
+        list_data = pd.DataFrame(data = [['x', 
+                                   'x','x', 0, 'x']],
+                                   columns=['anonStudentId','unit','problem',
+                                   'correctFirstAttempt','kc'])
+        return list_data
 
 clean_student_data()
--- sql_script.sql
+++ sql_script.sql
@@ -27,27 +27,17 @@ INTO TABLE edm.algebraTrain FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 DELETE FROM edm.algebraTrain where rowNumber = 0;
-
+drop table edm.algebraTrain_clean;
 create table edm.algebraTrain_clean
 (
-	rowNumber int,	
 	anonStudentId varchar(50),
     unit varchar(50),
     problem varchar(50),
-    problemName varchar(100),
-    problemView int,
-    stepName varchar(50),
-    stepStartTime varchar(50),
-    firstTransactionTime varchar(50),	
-    correctTransactionTime varchar(50),
-    stepEndTime varchar(50),	
-    stepDuration double,
-    correctStepDuration double,
-    errorStepDuration double,
     correctFirstAttempt int,
-    incorrects int,
-	hints int,
-    corrects int,
-	kc varchar(2000),
-    opportunity varchar(100)
+	kc varchar(2000)
 );
+
+use edm;
+
+create view algebraTrainSample2 as
+select * from algebraTrain limit 0,30;
